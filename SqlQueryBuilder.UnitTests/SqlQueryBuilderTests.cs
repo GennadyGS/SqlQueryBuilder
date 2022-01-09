9157083179abdec8ad00c0a8d13c994d6f6b3c7c
Gennadii Saltyshchak <GennadyGS@gmail.com>
2022-01-09 17:48:02 +0200

    Add test for composability

--- SqlQueryBuilder.UnitTests/SqlQueryBuilderTests.cs
+++ SqlQueryBuilder.UnitTests/SqlQueryBuilderTests.cs
@@ -9,34 +9,52 @@ public sealed class SqlQueryBuilderTests
     [Fact] 
     public void ShouldBeImplicitlyCastFromString()
     {
-        SqlQueryBuilder query = "SELECT * FROM Orders Id = 123";
+        SqlQueryBuilder query = "SELECT * FROM Orders WHERE Id = 123";
 
         query.Should()
-            .HaveQuery("SELECT * FROM Orders Id = 123")
+            .HaveQuery("SELECT * FROM Orders WHERE Id = 123")
             .And.NotHaveParameters();
     }
 
     [Fact]
     public void ShouldBeImplicitlyCastFromInterpolatedString()
     {
-        SqlQueryBuilder query = $"SELECT * FROM Orders Id = 123";
+        SqlQueryBuilder query = $"SELECT * FROM Orders WHERE Id = 123";
 
         query.Should()
-            .HaveQuery("SELECT * FROM Orders Id = 123")
+            .HaveQuery("SELECT * FROM Orders WHERE Id = 123")
             .And.NotHaveParameters();
     }
 
     [Fact]
     public void ShouldBeImplicitlyCastFromInterpolatedStringWithParameters()
     {
-        SqlQueryBuilder query = $"SELECT * FROM Orders Id = {123}";
+        SqlQueryBuilder query = $"SELECT * FROM Orders WHERE Id = {123}";
 
         query.Should()
-            .HaveQuery("SELECT * FROM Orders Id = @p1").
+            .HaveQuery("SELECT * FROM Orders WHERE Id = @p1").
             And.HaveParameters(
                 new Dictionary<string, object>
                 {
                     ["p1"] = 123,
+                });
+    }
+
+    [Fact]
+    public void ShouldBeComposable()
+    {
+        SqlQueryBuilder innerQuery = $"SELECT * FROM Orders WHERE Id = {123}";
+        SqlQueryBuilder outerQuery = $"SELECT * FROM ({innerQuery}) src WHERE IsValid = {true}";
+
+        outerQuery.Should()
+            .HaveQuery(
+                "SELECT * FROM (SELECT * FROM Orders WHERE Id = @p1) src" + 
+                " WHERE IsValid = @p2")
+            .And.HaveParameters(
+                new Dictionary<string, object>
+                {
+                    ["p1"] = 123,
+                    ["p2"] = true,
                 });
     }
 }
