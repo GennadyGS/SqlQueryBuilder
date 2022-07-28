7750875f670147a7af019ed0c87b2fa35f2e5131
Gennadii Saltyshchak <GennadyGS@gmail.com>
2022-07-28 18:36:19 +0300

    Make attribute class public

--- SqlQueryBuilder/Attributes/InterpolatedStringHandlerAttribute.cs
+++ SqlQueryBuilder/Attributes/InterpolatedStringHandlerAttribute.cs
@@ -1,6 +1,6 @@
 ﻿namespace System.Runtime.CompilerServices;
 
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, Inherited = false)]
-internal sealed class InterpolatedStringHandlerAttribute : Attribute
+public sealed class InterpolatedStringHandlerAttribute : Attribute
 {
 }
